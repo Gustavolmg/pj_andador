7ae1dc97d4eb7f011b84d4704ea90269cd048c25
Gustavo L M Gomes <gustavolmgomes@gmail.com>
2022-08-29 12:39:07 -0300

    teste

---
+++ app/php/config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+    const site_name = "Mercadinho budega";
+    const site_pcolor = "green";
+    const site_logo = "";
+
+    $dir_css = "\PJ-andador\app\css";
+    $dir_js = "\PJ-andador\app\js";
+    $die_img = "././asse/img"; 
+
+?>
